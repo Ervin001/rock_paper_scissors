ecbce972b9048d72fca50be37d4714595ce60f06
Ervin Gonzalez <evgnz24@gmail.com>
2022-04-04 17:58:35 -0500

    Added comments. \n and fixed the code for when it's a draw

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 'use strict';
 
+// Array that will let the computerPLay function choose an option
 const typesArr = ['rock', 'paper', 'scissors'];
 
+// Function that determines what the computer will output
 const computerPlay = function () {
+  // variable that randomly will pick from the three choices in the array
   let value = Math.floor(Math.random() * 3);
   // console.log(value);
 
   return typesArr[value];
 };
 
+//
 function playRound(playerSelection, computerSelection) {
-  const player = playerSelection;
-  const comp = computerSelection;
+  console.log(playerSelection, computerSelection);
 
-  if (player === !comp && player === !) {
-    console.log(`${player} and ${comp} are the same`);
+  // Var for playerSelection and turned lower case
+  const choicePlayer = playerSelection.toLowerCase();
+
+  // console.log(choicePlayer);
+
+  // Checks if it's a tie
+  if (choicePlayer === computerSelection) {
+    console.log('Its a tie!');
   }
 }
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
 
-playRound('rock', computerSelection);
+// playRound('RoCk', computerPlay());
+playRound('Paper', computerPlay());
+playRound('Scissors', computerPlay());

eb23014ef7a214b740a683b47b9c1d8d256fa8fe
Ervin Gonzalez <evgnz24@gmail.com>
2022-04-03 23:54:02 -0500

    Worked on some of the javascript: Added a Function that has the computer pick from an array of 3 choices and returns one result every time.
    Made another function that has not been finished: This function will tell who won the player or the computer in Rock Paper Scissors.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const typesArr = ['rock', 'paper', 'scissors'];
+
+const computerPlay = function () {
+  let value = Math.floor(Math.random() * 3);
+  // console.log(value);
+
+  return typesArr[value];
+};
+
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection;
+  const comp = computerSelection;
+
+  if (player === !comp && player === !) {
+    console.log(`${player} and ${comp} are the same`);
+  }
+}
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+
+playRound('rock', computerSelection);

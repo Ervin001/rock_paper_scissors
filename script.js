5bfb8f143b2e3039634257719f792ec02bc2b2b6
Ervin Gonzalez <evgnz24@gmail.com>
2022-04-14 12:08:35 -0500

    Added buttons and some score logic

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 'use strict';
 
+const buttons = document.querySelectorAll('button');
+const resultEl = document.querySelector('.results');
+const playerScoreEl = document.querySelector('.score--player');
+const compScoreEl = document.querySelector('.score--comp');
+
 // Array that will let the computerPlay function choose an option
 const typesArr = ['rock', 'paper', 'scissors'];
+const scores = [0, 0];
+let playerScore = 0;
+let compScore = 0;
 
 // Function that determines what the computer will output
 const computerPlay = function () {
@@ -23,18 +31,40 @@ function playRound(playerSelection, computerSelection) {
   if (choicePlayer === computerSelection) {
     return 'Its a tie!';
   } else if (choicePlayer === 'scissors' && computerSelection === 'rock') {
+    compScore++;
     return `You lose, ${computerSelection} beats ${playerSelection}`;
   } else if (choicePlayer === 'scissors' && computerSelection === 'paper') {
+    playerScore++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (choicePlayer === 'rock' && computerSelection === 'scissors') {
+    playerScore++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (choicePlayer === 'rock' && computerSelection === 'paper') {
+    compScore++;
     return `You lose, ${computerSelection} beats ${playerSelection}`;
   } else if (choicePlayer === 'paper' && computerSelection === 'rock') {
+    playerScore++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (choicePlayer === 'paper' && computerSelection === 'scissors') {
+    compScore++;
     return `You lose, ${computerSelection} beats ${playerSelection}`;
   }
 }
 
-const playerSelection = 'rock';
+// console.log(playRound('rock', computerPlay()));
+
+buttons.forEach((btn) =>
+  btn.addEventListener('click', function (e) {
+    resultEl.textContent = '';
+
+    resultEl.textContent = playRound(e.target.textContent, computerPlay());
+    scores[0] = playerScore;
+
+    playerScoreEl.textContent = scores[0];
+
+    scores[1] = compScore;
+
+    compScoreEl.textContent = scores[1];
+    // console.log(scores);
+  })
+);
